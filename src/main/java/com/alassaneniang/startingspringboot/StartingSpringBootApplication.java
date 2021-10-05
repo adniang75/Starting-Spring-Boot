6fe557620cf0bec5ade51c3ea98a5777ae0b2632
Alassane NIANG <adniang75@gmail.com>
2021-10-05 14:32:37 +0200

    Initial commit

---
+++ src/main/java/com/alassaneniang/startingspringboot/StartingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.alassaneniang.startingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingSpringBootApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run( StartingSpringBootApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/alassaneniang/startingspringboot/StartingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alassaneniang.startingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartingSpringBootApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
